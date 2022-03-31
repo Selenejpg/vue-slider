37d5996a41dc350e2b8c660331c41ce4475dc0d0
noemipintaldi <noemipintaldi@gmail.com>
2022-03-31 16:34:22 +0200

    progressi fino ad ora

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        immagini: ['1_Gsq1yHsb3cV2JKuj5c_SoQ.jpeg','luca-bravo-o453m2liufs-unsplash-1.jpeg','NIK95.lead_.GettyImages_1037349126.jpg', 'Sudafrica_fynbos_1.webp']
+    }
+    
+})
